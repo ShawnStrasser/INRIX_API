baecca44024b90c7cab27f6f720ca7dcf0d7c24b
Shawn Strasser <shawn.strasser@odot.state.or.us>
2021-02-18 15:14:45 -0800

    Added credential handling, restructured with functions, and added daily download scheduling ability wit new module

---
+++ INRIX_XD_Daily_Downloader.py
@@ -0,0 +1,26 @@
+## This downloads daily INRIX XD segemnt data, it is intened to be run using the Windows Task Scheduler
+## Disclaimer: use at your own risk, no assumptions of liability or otherwise and so forth etc. 
+
+
+import INRIX_XD_Downloader as d
+import datetime
+
+last_run_file = d.folder + 'last_run.txt'
+d.xd_segment_filename = 'daily_download_segments.txt' 
+today = datetime.datetime.now().date()
+
+with open(last_run_file, 'r') as file:
+    last_run = file.read()
+
+last_run = datetime.datetime.strptime(last_run, '%Y-%m-%d').date()
+
+while last_run < today - datetime.timedelta(days=1):
+    d.start_date = str(last_run + datetime.timedelta(days=1))
+    d.end_date = d.start_date
+    print('\nWORKING ON: ' + d.start_date)
+    d.main()
+    with open(last_run_file, 'w') as file:
+        file.write(d.start_date)
+    last_run += datetime.timedelta(days=1)
+
+print('=' * 100 + '\nDaily download complete\n' + '=' * 100)
